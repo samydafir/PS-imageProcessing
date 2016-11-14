ed063d5e6e6eb632644cf8731f834f4779243bf2
sebbsn <sebastian.strumegger@stud.sbg.ac.at>
2016-11-14 10:26:56 +0100

    canny, sobelX, sobelY

--- src/EdgeDetection.java
+++ src/EdgeDetection.java
@@ -21,7 +21,7 @@ public class EdgeDetection {
 	
 	public static void detect(String srcPath, String dstPath, int highThresh, int lowThresh) throws IOException {
 		
-		Mat src, sobX, sobY, lines, edge, im;
+		Mat src, sobX, sobY, edge;
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 				
@@ -29,42 +29,22 @@ public class EdgeDetection {
 		sobX = new Mat();
 		sobY = new Mat();
 		edge = new Mat();
-		lines = new Mat();
-		im = new Mat();
 		
 		
 		src = Imgcodecs.imread(srcPath);
 		edge.create( src.size(), src.type() );
-		lines.create( src.size(), src.type() );
-		im.create( src.size(), src.type() );
 		
 		
 		Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.blur(src, src, new Size(3, 3));
 		Imgproc.Sobel(src, sobX, src.depth(), 1, 0);
 		Imgproc.Sobel(src, sobY, src.depth(), 0, 1);
-		Imgproc.Canny(src, edge, lowThresh, highThresh);
-		Imgproc.HoughLines(edge, lines, 1, Math.PI/180, 10);
-		
-		
-		for (int i = 0; i < lines.cols(); i++){
-			double data[] = lines.get(0, i);
-			double rho = data[0];
-			double theta = data[1];
-			double cosTheta = Math.cos(theta);
-			double sinTheta = Math.sin(theta);
-			double x0 = cosTheta * rho;
-			double y0 = sinTheta * rho;
-			Point pt1 = new Point(x0 + 10000 * (-sinTheta), y0 + 10000 * cosTheta);
-			Point pt2 = new Point(x0 - 10000 * (-sinTheta), y0 - 10000 * cosTheta);
-			Imgproc.line(im, pt1, pt2, new Scalar(0, 0, 200), 3);
-}
+		Imgproc.Canny(src, edge, lowThresh, highThresh);		
 		
 
 		Imgcodecs.imwrite(dstPath + "_canny.jpg", edge);
 		Imgcodecs.imwrite(dstPath + "_sobelX.jpg", sobX);
 		Imgcodecs.imwrite(dstPath + "_sobelY.jpg", sobY);
-		Imgcodecs.imwrite(dstPath + "_houghLines1.jpg", im);
 		
 		
 	}
