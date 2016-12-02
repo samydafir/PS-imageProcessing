3d7a70c5ccebb80f0483ca79c954f26bd12df61f
sebbsn <sebastian.strumegger@stud.sbg.ac.at>
2016-12-02 23:59:31 +0100

    First step for Histogram class, added Laplacian algorithm in EdgeDection test class

--- src/EdgeDetection.java
+++ src/EdgeDetection.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import java.util.Random;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -13,15 +15,19 @@ public class EdgeDetection {
 
 	public static void main(String[] args) throws IOException {
 		
-		detect("examples/edge/src1.jpg", "/home/sebastian/Documents/Uni/imgproc/dst1", 60, 60/3);
-		detect("examples/edge/src2.jpg", "/home/sebastian/Documents/Uni/imgproc/dst2", 100, 100/3);
+		int thresh1 = 60;
+		int thresh2 = 100;
+		
+		canny("examples/edge/src1.jpg", "/home/sebastian/Documents/Uni/imgproc/dst1", thresh1, thresh1/3);
+		canny("examples/edge/src2.jpg", "/home/sebastian/Documents/Uni/imgproc/dst2", thresh2, thresh2/3);
+		canny("examples/edge/src3.jpg", "/home/sebastian/Documents/Uni/imgproc/dst3", thresh2, thresh2/3);
 		
 	}
 	
 	
-	public static void detect(String srcPath, String dstPath, int highThresh, int lowThresh) throws IOException {
+	public static void canny(String srcPath, String dstPath, int highThresh, int lowThresh) throws IOException {
 		
-		Mat src, sobX, sobY, edge;
+		Mat src, sobX, sobY, edge, laplace, morphologyEx;
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 				
@@ -29,22 +35,25 @@ public class EdgeDetection {
 		sobX = new Mat();
 		sobY = new Mat();
 		edge = new Mat();
-		
+		laplace = new Mat();
 		
 		src = Imgcodecs.imread(srcPath);
-		edge.create( src.size(), src.type() );
+		edge.create(src.size(), src.type());
 		
 		
 		Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.blur(src, src, new Size(3, 3));
 		Imgproc.Sobel(src, sobX, src.depth(), 1, 0);
 		Imgproc.Sobel(src, sobY, src.depth(), 0, 1);
-		Imgproc.Canny(src, edge, lowThresh, highThresh);		
+		Imgproc.Canny(src, edge, lowThresh, highThresh);	
+		Imgproc.Laplacian(src, laplace, src.depth(), 3, 1, 0);
 		
 
 		Imgcodecs.imwrite(dstPath + "_canny.jpg", edge);
 		Imgcodecs.imwrite(dstPath + "_sobelX.jpg", sobX);
 		Imgcodecs.imwrite(dstPath + "_sobelY.jpg", sobY);
+		Imgcodecs.imwrite(dstPath + "_laplacian.jpg", laplace);
+		Imgcodecs.imwrite(dstPath + "_test.jpg", test);
 		
 		
 	}
---
+++ src/EdgeHistogram.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+
+/*
+ * NOTE: don't forget to load the opencv library before using this class:  System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+ */
+
+public class EdgeHistogram {
+
+	private String srcPath;
+	
+	
+	private Mat srcMat;		// source Mat
+	private Mat cannyMat;	// Mat that contains the edge detected image
+	private Mat lengthMat;	// Mat that contains the lengths of the edges
+	private Mat histMat;	// final Mat that contains the histogram
+	private Mat mask;		// mask can be used for histogram calculation, if we don't want to take a histogram of the full pic
+	
+	private int highThresh;	// high threshold for canny edge detection
+	private int lowThresh;	// low threshhold
+	
+	private final int nrOfBins;			// Number of Bins used for the histogram
+	private final float maxEdgeLength;	// Max value that should be considered in the histogram
+	
+	private List<Mat> histMatList;
+	
+	
+	public EdgeHistogram(String srcPath, int nrOfBins, int maxEdgeLength, int highThresh, int lowThresh) {
+		
+		this.nrOfBins = nrOfBins;
+		this.highThresh = highThresh;
+		this.lowThresh = lowThresh;
+		this.maxEdgeLength = maxEdgeLength;
+		
+		this.srcPath = srcPath;
+		this.srcMat = new Mat();
+		this.cannyMat = new Mat();
+		this.lengthMat = new Mat();
+		this.mask = new Mat();
+		
+		this.srcMat = Imgcodecs.imread(srcPath);
+		cannyMat.create(srcMat.size(), srcMat.type());
+		
+		histMatList = new ArrayList<Mat>();
+	}
+	
+	private void extractEdges() {
+		Imgproc.cvtColor(srcMat, cannyMat, Imgproc.COLOR_RGB2GRAY);
+		Imgproc.blur(cannyMat, cannyMat, new Size(3, 3));
+		Imgproc.Canny(cannyMat, cannyMat, lowThresh, highThresh);
+		histMatList.add(cannyMat);
+		
+		getEdgeLengths();
+	}
+	
+	private void getEdgeLengths() {
+		// TODO: save edge lengths in lengthMat
+	}
+	
+	public void calcHistogram() {
+		extractEdges();
+		Imgproc.calcHist(histMatList, new MatOfInt(0), mask, histMat, new MatOfInt(nrOfBins), new MatOfFloat(0f, this.maxEdgeLength));
+	}
+	
+	public Mat getHistogram() {
+		return this.histMat;
+	}
+}
